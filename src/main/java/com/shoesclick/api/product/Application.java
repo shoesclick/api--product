79d4cca10e359999ec849ac0357c2ba4559082dc
git <claytnmorais@gmail.com>
2024-03-22 00:38:02 -0300

    apis com integração com keycloak

--- src/main/java/com/shoesclick/api/product/Application.java
+++ src/main/java/com/shoesclick/api/product/Application.java
@@ -2,7 +2,11 @@ package com.shoesclick.api.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@EnableMethodSecurity
+@EnableWebSecurity
 @SpringBootApplication
 public class Application {
 
